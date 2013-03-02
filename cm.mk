004d98391a56baf252ae92eeba7077e8acda5338
angelsl <hidingfromhidden@gmail.com>
2013-03-02 16:16:20 +0800

    stuttgart: Actually remove init.rc.
    Change-Id: I1c7c7b07f4ab87807ee250f0b59c5def8e2fdeb4
    Signed-off-by: angelsl <hidingfromhidden@gmail.com>

--- cm.mk
+++ cm.mk
@@ -41,7 +41,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/init.rc:root/init.rc \
     $(LOCAL_PATH)/init.smdk4x12.rc:root/init.smdk4x12.rc \
     $(LOCAL_PATH)/init.smdk4x12.usb.rc:root/init.smdk4x12.usb.rc \
     $(LOCAL_PATH)/ueventd.smdk4x12.rc:root/ueventd.stuttgart.rc \
